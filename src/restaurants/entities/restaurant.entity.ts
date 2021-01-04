00897f11f55d9985e732041a6dd6ed5f760d76c8
LJ <jhlee9193@gmail.com>
2021-01-04 22:38:36 +0900

    Our First Entity

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,15 +1,26 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Field, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
+@Entity()
 export class Restaurant {
+  @PrimaryGeneratedColumn()
+  @Field((type) => Number)
+  id: number;
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   @Field((type) => String)
+  @Column()
   name: string;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   @Field((type) => Boolean, { nullable: true })
+  @Column()
   isVegan?: boolean;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   @Field((type) => String)
+  @Column()
   address: string;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   @Field((type) => String)
+  @Column()
   ownersName: string;
 }
